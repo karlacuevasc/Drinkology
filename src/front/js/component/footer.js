4105896d3146ca461550982b890c08d3853de7f4
Kcuevas95 <karlacuevasdesigns@gmail.com>
2021-09-06 03:12:04 +0000

    Removed sign up tab from the navbar

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-6 py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> Website By
-			<a href="https://github.com/Kcuevas95"> Karla</a>,<a href="https://github.com/allenstfort23"> Allen</a>, &
-			<a href="https://github.com/luigifurlani13"> Luis</a>
-		</p>
+		<p>footer</p>
 	</footer>
 );
