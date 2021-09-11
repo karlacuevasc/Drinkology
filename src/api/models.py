9116c43293d155b9485dbd63a1b70cdb843e574e
Kcuevas95 <karlacuevasdesigns@gmail.com>
2021-09-11 01:04:47 +0000

    Created data models

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,62 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    favorite_cocktails = db.relationship('FavoriteCocktail', backref='user', lazy=True)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return self.first_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+            # do not serialize the password, its a security breach
+        }
+
+
+class Cocktail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    instructions = db.Column(db.String(120), unique=True, nullable=False)
+    ingredients = db.Column(db.String(120), unique=True, nullable=False)
+    measurements= db.Column(db.String(120), unique=True, nullable=False)
+    favorite_cocktails = db.relationship('FavoriteCocktail', backref='cocktail', lazy=True)
+   
+    
+
+    def __repr__(self):
+        return self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "instructions": self.instructions,
+            "ingredients": self.ingredients,
+            "measurements": self.measurements
+            # do not serialize the password, its a security breach
+        }
+
+class FavoriteCocktail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    cocktail_id = db.Column(db.Integer, db.ForeignKey('cocktail.id'), nullable=False)
+   
+
+    def __repr__(self):
+        return self.user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "instructions": self.instructions,
+            "ingredients": self.ingredients,
+            "measurements": self.measurements
             # do not serialize the password, its a security breach
         }
